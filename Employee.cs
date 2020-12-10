87a8296e8a75a6138b1ff5e210ec28fd6923569d
Mark Curran <S00199895@mail.itsligo.ie>
2020-12-10 10:53:09 +0000

    Created the abstarct class Employee

---
+++ Employee.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CA2_S00199895
+{
+    public abstract class Employee
+    {
+        public abstract string FirstName { get; set; }
+        public abstract string LastName { get; set; }
+
+        public abstract decimal CalculateMonthlyPay();
+    }
+}
